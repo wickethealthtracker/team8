f3a27f2d72d4cac08bd5baf92482d0364bbf9e2c
litehosue43 <lightholder.jack16@gmail.com>
2014-03-24 15:30:50 -0700

    Create SplashScreenPanel.java

---
+++ SplashScreenPanel.java
@@ -0,0 +1,14 @@
+/**
+ * Auto Generated Java Class.
+ */
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class SplashScreenPanel extends JPanel {
+  
+
+  public SplashScreenPanel() {
+    this.add(new JLabel("splash screen"));
+  }
+  
+}
