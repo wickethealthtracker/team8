003390106afdc1b95a9718ac612dee4682fb4935
litehosue43 <lightholder.jack16@gmail.com>
2014-03-24 15:31:48 -0700

    Create SplashScreenPanel.java

---
+++ SplashScreenPanel.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class SplashScreenPanel extends JPanel {
+  
+
+  public SplashScreenPanel() {
+    this.add(new JLabel("splash screen"));
+  }
+  
+}
