966f0f54008a57ce3569d096bb512c5dc2652fc9
litehosue43 <lightholder.jack16@gmail.com>
2014-05-02 17:29:41 -0700

    Create MetricType.java

---
+++ MetricType.java
@@ -0,0 +1,18 @@
+/**
+ * Author Jack
+ * 
+ * The MetricType enumeration gives a list of the possible metric
+ * types one can perform.
+ * 
+ */
+
+//Project package
+package cse_360_project;
+
+public enum MetricType {
+    BODY_MASS_PERCENTAGE,
+    WEIGHT,
+    HEIGHT_INCHES,
+    RESTING_HEART_RATE,
+    ACTIVE_HEART_RATE
+}
