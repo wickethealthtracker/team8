7ed3971c5ffec45b55d4120a335b71c641f68e12
litehosue43 <lightholder.jack16@gmail.com>
2014-05-02 17:32:37 -0700

    Create WorkoutType.java

---
+++ WorkoutType.java
@@ -0,0 +1,18 @@
+/**
+ * Author Jack
+ * 
+ * The WorkoutType enumeration gives a list of the possible workout
+ * types one can perform.
+ */
+
+
+//Project package
+package cse_360_project;
+
+public enum WorkoutType {
+    WEIGHTS_MINUTES,
+    RUNNING_MINUTES,
+    SWIMMING_MINUTES,
+    CROSSFIT_MINUTES,
+    CYCLING_MINUTES
+}
